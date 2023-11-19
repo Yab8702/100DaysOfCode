e9939b28a260eafcffee33d54fe78072de82108a
leole <fetamasr@gmail.com>
2023-11-20 00:33:40 +0300

    weather forcasting web app using react

---
+++ Projects/liull-4r/Day--3--Weather-Forcsting-with-some-style/src/App.jsx
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import WeatherApp from "./Components/Weather";
+
+function App() {
+  return (
+    <>
+      <WeatherApp />
+    </>
+  );
+}
+
+export default App;
